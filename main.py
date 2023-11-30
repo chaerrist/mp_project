7e256b0c83ab3d6e24026576c386c9b3badd00e7
chaerang <apotheosis408@naver.com>
2023-11-30 15:26:33 +0900

    파일 생성

---
+++ main.py
@@ -0,0 +1,10 @@
+from calculation.calc import control_data
+from excel.input import open_excel
+from excel.output import write_excel
+
+def main():
+    year = 2022
+    result = []
+    
+if __name__ == '__main__':
+    main()
