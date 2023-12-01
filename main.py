5828f2578c297263bee63480c1f03235a991e194
chaerang <apotheosis408@naver.com>
2023-12-01 12:06:19 +0900

    메인 파일 작성

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from excel.output import write_excel
 def main():
     year = 2022
     result = []
+    for month in range(1, 12):
+        data = open_excel(2022, month)
+
+        # 계산하는 function 호출
+        count = 1
+
+        result.append([str(month) + "월", count])
+
+    success = write_excel(result)
+    if success == True:
+        print("success!")
+    else:
+        print("fail...")
     
 if __name__ == '__main__':
     main()
